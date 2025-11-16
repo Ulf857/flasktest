2171a58bf998c47384854fbc26adf061814e9231
nosterulf <nosterulf@gmail.com>
2025-11-16 14:43:03 +0000

    Route mit Ausgabe der Zeit zugefügt.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+import pytz
 
 from flask import Flask, send_file
 
@@ -7,6 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return send_file('src/index.html')
+
+@app.route("/time")
+def time():
+    berlin_tz = pytz.timezone("Europe/Berlin")
+    berlin_time = datetime.datetime.now(berlin_tz)
+    return berlin_time.strftime("%d.%m.%Y, %H:%M:%S Uhr")
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
